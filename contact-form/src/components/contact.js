728a208dcb53ff0a64c3e59a4a1f2df1d59dc647
Muthusmart <muthukumardigisailor@gmail.com>
2022-02-02 15:33:33 +0530

    commit message

---
+++ contact-form/src/components/contact.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "../app.css";
+
+const Contact =()=>{
+    return(
+       
+<form className="form">
+<h1> Contact Us🎧 </h1>
+<label> Name🔥</label>
+<input placeholder="Your Name" />
+<label>Email📧</label>
+<input placeholder="Your Email"/>
+<label>Message📲
+
+</label>
+<textarea></textarea>
+<button type="submit" >submit</button>
+
+
+</form>
+
+);
+    }  ;
+export default Contact;
